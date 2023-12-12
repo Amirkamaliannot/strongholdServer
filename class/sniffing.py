b4c35adfc9459c598462f3c87fee0427a1fce392
amir <amirkamalian24@gmail.com>
2023-12-12 21:47:59 +0330

    adding port to Packet class \ adding sniffing class

--- class/sniffing.py
+++ class/sniffing.py
@@ -1,6 +1,11 @@
 import socket
 import struct
 import time
+import random
+from enum import Enum
+
+
+
 
 class Packet:
     def __init__(self, packet):
@@ -21,6 +26,9 @@ class Packet:
         self.destination_address = socket.inet_ntoa(self.header[9])
         # payload data starts after the header
         self.payload = packet[self.header_length:self.total_length]
+
+        self.source_port = int.from_bytes(packet[self.header_length: self.header_length+2] , 'big')
+        self.destination_port = int.from_bytes(packet[self.header_length+2: self.header_length+4], 'big')
 
 
     def calculate_checksum(self):
@@ -56,41 +64,84 @@ class Packet:
         self.header = struct.unpack('!BBHHHBBH4s4s', self.header_raw)
         self.calculate_checksum()
 
-
     def output_packet(self):
         return self.header_raw + self.payload
 
 
+class Sniffing:
+    def __init__(self, localIP):
 
-#######
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-local =s.getsockname()[0]
-s.close()
-del s
-#######
-
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-
-s.bind((local,0))
+        self.localIP = localIP
+        self.socket = None
+        self.creat_socket()
 
 
+    def creat_socket(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+        s.bind((self.localIP,0))
 
-# Include IP headers
-s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+        # Include IP headers
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
-# receive all packages
-s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+        # receive all packages
+        s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+        self.socket = s
+
+    def sniffing(self):
+        while True:
+            data = self.socket.recvfrom(65565)
+            packet = Packet(data[0])
+
+            if(self.filter_game_packets(packet)):
+                yield packet
+
+    def filter_game_packets(self, packet:Packet):
+
+        if(packet.protocol == socket.IPPROTO_UDP or packet.protocol == socket.IPPROTO_TCP):
+            if( 
+                2400 < packet.destination_port <= 2400 or
+                2400 < packet.source_port <= 2400 or
+                packet.source_port == 47624 or 
+                packet.destination_port  == 47624 or 
+                packet.source_port == 443
+            ):
+                
+                return True 
+        return False
 
 
-while True:
-    print('waiting')
-    time.sleep(0.0001)
+class Player:
+    def __init__(self):
+        pass
 
-    # receive a package
-    data = s.recvfrom(65565)
-    print('recive')
-    address = data[1]
-    packet = Packet(data[0])
-    break
+
+
+def getLocalIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    local =s.getsockname()[0]
+    s.close()
+    del s
+    return local
+
+
+def getRandomLocalIP(lacal_ip):
+    ip_ng = lacal_ip.split('.')[:3] + [str(random.randint(100, 255))]
+    return ".".join(ip_ng)
+
+
+LocalIP = getLocalIP()
+sniffing = Sniffing(LocalIP)
+
+for i in sniffing.sniffing():
+    print(i.source_address)
+    print(i.destination_address)
+    print(i.header_checksum)
+    print(i.total_length)
+    print(i.payload)
+    print(i.source_port)
+    print(i.destination_port)
+    print(i.output_packet())
+    # break
+
 
